d2067f6f8e396d8218db9893439d9280ecf23223
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-14 00:45:39 +0200

    Pass Hydra entry flag to location trainings

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260713-unifiedstyle3";
+const CACHE_NAME = CACHE_PREFIX + "20260714-hydra-entry-splash1";
 
 const CORE_ASSETS = [
   "./",
