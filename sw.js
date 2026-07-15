465d57e0c3cc9c7f6a8a2718c7b57dc0b2c4c184
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-15 14:26:04 +0200

    Napraw blokowanie scrolla przez loader

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260715-frontend-master1-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260715-loader-hotfix1-hydra";
 
 const CORE_ASSETS = [
   "./",
