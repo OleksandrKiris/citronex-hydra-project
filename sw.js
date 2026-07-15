620ff7e869dc1b03d0ff725bc9cc7128e796edd8
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-15 12:04:03 +0200

    Improve Hydra visual polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260714-hydra-phonefit2";
+const CACHE_NAME = CACHE_PREFIX + "20260715-hydra-visual-polish1";
 
 const CORE_ASSETS = [
   "./",
