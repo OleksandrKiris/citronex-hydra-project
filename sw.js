c27729c32a0eeacc67e009b718b6e635d2fe6023
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-15 12:21:10 +0200

    Improve Hydra mobile frontend polish

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260715-hydra-visual-polish1";
+const CACHE_NAME = CACHE_PREFIX + "20260715-frontend-master1-hydra";
 
 const CORE_ASSETS = [
   "./",
