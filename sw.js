de77b2685e33c33a3e3c8c435329e17d0a22b3a7
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-16 14:23:04 +0200

    Use browser language by default

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260716-dragon-guide1-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260716-browser-lang1-hydra";
 
 const CORE_ASSETS = [
   "./",
