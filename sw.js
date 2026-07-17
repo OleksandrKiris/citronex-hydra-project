d30f87a79cb16ed07a535abc0e31c92f39a7f1fa
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-17 08:36:22 +0200

    Improve dragon voice diction

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260717-voice2-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260717-voice3-hydra";
 
 const CORE_ASSETS = [
   "./",
