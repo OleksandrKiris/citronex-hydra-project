56acf3d0d8cf0cb00fdb5449e4d18015ff5b802e
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-17 08:57:56 +0200

    Refine Hydra color system

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260717-voice3-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260717-style2-hydra";
 
 const CORE_ASSETS = [
   "./",
