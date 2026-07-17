e0b037f49602060fb9648c8ddc6e9f00c5c0e371
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-17 09:26:14 +0200

    Improve Hydra voice diction

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260717-style2-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260717-voice5-hydra";
 
 const CORE_ASSETS = [
   "./",
