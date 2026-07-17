f6ccdcba3162ea516743de78c09493881ffc1bbd
Oleksandr Kiris <okiris@ad.pposiechnice.pl>
2026-07-17 10:28:19 +0200

    Fix direct location launch flow

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260717-voice7-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260717-direct-locations-hydra";
 
 const CORE_ASSETS = [
   "./",
