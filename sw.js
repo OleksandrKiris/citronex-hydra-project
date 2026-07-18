388ac5c50ec3880499292b748d3c3ee0c8847705
Oleksandr <137423222+OleksandrKiris@users.noreply.github.com>
2026-07-18 07:15:14 +0200

    Connect standalone greenhouse 3D project
    Adds the separate greenhouse 3D link without embedding the original enterprise 3D project.

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = "citronex-hydra-srzb-";
-const CACHE_NAME = CACHE_PREFIX + "20260717-direct-locations-hydra";
+const CACHE_NAME = CACHE_PREFIX + "20260718-greenhouse-link";
 
 const CORE_ASSETS = [
   "./",
